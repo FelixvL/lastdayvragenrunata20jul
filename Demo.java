5b1f15b5f11b8bc844a481d4c71c590cfad3397e
FelixvL <digitaleservice@gmail.com>
2020-08-25 15:09:31 +0200

    BA

--- Demo.java
+++ Demo.java
@@ -1,26 +1,11 @@
-class Demo { // HR
+class Demo {  // BA
 	public static void main(String... args) {
-		System.out.println("regen");
-		Dier koe = new Koe();
-		Boot dier = (Boot)koe;
+		int array = new int[];
+		System.out.println("einde");
 	}	
 
 }
-class Koe extends Dier{
-	int leeftijd = 23;
-	int melken(){
-		System.out.println("gaos");
-		return 14;
-	}
-}
-class Dier{
-	int leeftijd = 25;
-	void ademen(){
-		System.out.println("ademen"+leeftijd);
-	}
-}
-class Boot{
-}
+
 
 
 
