6b3779344349290383e42515868b481a1a33c8ad
FelixvL <digitaleservice@gmail.com>
2020-08-25 09:09:56 +0200

    HT

--- Demo.java
+++ Demo.java
@@ -1,10 +1,16 @@
-class Demo { // HR
-	public static void main(String jojo...) {
-		System.out.println("hallo");
+class Demo { // HT
+	public static void main(String... args) {
+		System.out.println("ola");
 		lopen();
 	}	
-	static void lopen(){
-		System.out.println("hij doet het");
+	static void lopen(int a){
+		System.out.println("hij doet het1");
+	}
+	static void lopen(short b){
+		System.out.println("hij doet het2");
+	}
+	static void lopen(double... b){
+		System.out.println("hij doet het3");
 	}
 }
 
