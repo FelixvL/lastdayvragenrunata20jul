fc838314e1b8e7523f6303161a90664f1c2d6b5c
FelixvL <digitaleservice@gmail.com>
2020-08-25 08:56:49 +0200

    HN

--- Demo.java
+++ Demo.java
@@ -1,17 +1,7 @@
-import java.util.Scanner;
-class Demo {
-	public static void main(String[] args) {
-		lopen();
-		Scanner sc = new Scanner(System.in);
-		String ingegeven  = sc.nextLine();
-		System.out.println(ingegeven);
-
-	}
-	
-	static void lopen() {
-		System.out.println("Ik wil hier komen");
-	}
-	
+class Demo { // HN
+	public final static void main(String... jojo) {
+		System.out.println("doei");
+	}	
 }
 
 
