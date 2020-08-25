b325b65f718d331d342dc06de404231947725c97
FelixvL <digitaleservice@gmail.com>
2020-08-25 16:33:58 +0200

    CM

--- Demo.java
+++ Demo.java
@@ -1,13 +1,20 @@
-class Demo {  // CK
+class Demo {  // CM
 	public static void main(String... args) throws Throwable{
 		Fruit f = new Fruit();
-		f.groeien();
+		try{
+			f.groeien();
+		catch(Exception e){
+			System.out.println("zie je niet");
+		}finally{
+			System.out.println("zie je nooit");
+		}
 		System.out.println("einde");
 	}	
 }
 class Fruit{
 	void groeien() throws Exception{
 		System.out.println("ik groei");
+		throw new Error();
 	}
 }
 
