642d5a9ac9fe7e091358ca3c74e8b23692a01b61
FelixvL <digitaleservice@gmail.com>
2020-08-25 15:25:18 +0200

    BI

--- Demo.java
+++ Demo.java
@@ -1,10 +1,21 @@
-class Demo {  // BF
+class Demo {  // BI
 	public static void main(String... args) {
-		//float r = 3.2;
-		short g = 6;
+		
+		Kip k = new Kip();
+		k.waggelen(6);
 		
 	}	
 
+}
+
+class Kip{
+	void waggelen(){
+		System.out.println("waggelen1");
+	}
+	int waggelen(short t){
+		System.out.println("waggelen2");
+		return 3;
+	}
 }
 
 
