243811b897625598508f297cb81fb3eb728cf5ab
FelixvL <digitaleservice@gmail.com>
2020-08-25 15:44:36 +0200

    BQ

--- Demo.java
+++ Demo.java
@@ -1,16 +1,16 @@
-class Demo {  // BP
+class Demo {  // BQ
 	public static void main(String... args) {
 		
 		
 	}	
 }
 
-class AAA{			
+class AAA implements III{			
 
 
 }
 
-class BBB extends AAA implements III{		
+class BBB extends AAA {		
 	public AAA dghi(){
 		return new BBB();
 	}
