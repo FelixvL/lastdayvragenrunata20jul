7ddd3dfcc80205f9c908d2faa7863e3e2c4ca534
FelixvL <digitaleservice@gmail.com>
2020-08-25 08:48:13 +0200

    go

---
+++ Demo.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Demo {
+	public static void main(String[] args) {
+		lopen();
+		Scanner sc = new Scanner(System.in);
+		String ingegeven  = sc.nextLine();
+		System.out.println(ingegeven);
+
+	}
+	
+	static void lopen() {
+		System.out.println("Ik wil hier komen");
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
