508d23e5919fe1568bdc26fd5f9d042c5c9bd411
FelixvL <digitaleservice@gmail.com>
2020-08-25 16:23:28 +0200

    CH

--- Demo.java
+++ Demo.java
@@ -1,14 +1,12 @@
-class Demo {  // CF
+class Demo {  // CH
 	public static void main(String... args) {
 		Fruit f = new Fruit();
-		f.groeien();
 		System.out.println("einde");
 	}	
 }
 class Fruit{
-	void groeien() throws RuntimeException{
+	void groeien() throws Exception{
 		System.out.println("ik groei");
-		throw new RuntimeException();
 	}
 }
 
