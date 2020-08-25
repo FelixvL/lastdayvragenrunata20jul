72420524986b99b79b02092addf4c5a141e525cd
FelixvL <digitaleservice@gmail.com>
2020-08-25 15:57:25 +0200

    BV

--- Demo.java
+++ Demo.java
@@ -1,25 +1,29 @@
-class Demo {  // BR
+class Demo {  // BV
 	public static void main(String... args) {
-		
+		JJJ j = new BBB();
 		
 	}	
 }
 
-class AAA implements III{			
-	public AAA dghi(){
-		return new BBB();
+class AAA implements III{  			
+	public void lopen(){
+
 	}
-
 }
 
-class BBB extends AAA {		
+class BBB extends AAA implements JJJ{	 	
+	public void fietsen(){
 
+	}
 }
 
-interface III{
-	III dghi();
+interface III{  
+	void lopen();
 }
 
+interface JJJ{
+	void fietsen();
+}
 
 
 
