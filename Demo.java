b85ac7a3011f1c4ca3ac883d07b95c8f9b90b4f7
FelixvL <digitaleservice@gmail.com>
2020-08-26 09:56:37 +0200

    JB

--- Demo.java
+++ Demo.java
@@ -1,27 +1,16 @@
-class Demo {  // IH
+class Demo {  // JB
 	public static void main(String... args){
-		Mountainbike fiets = new DirtBike();
-		fiets.fietsen(25);
-		System.out.println("einde");
+		Raam raam = new Raam();
+		raam.kijken();
 	}	
 }
-class Fiets{
-	int a = 35;
-	long fietsen(){
-		int a = 25;
-		System.out.println(a);
-		return 9;
+class Raam implements Doorzichtig{
+	static void kijken(){
+		System.out.println("Kijken in raam");
 	}
 }
-abstract class Mountainbike extends Fiets{
-	int a = 37;
-	abstract int fietsen(int c);
-}
-class DirtBike extends Mountainbike{
-	int fietsen(int d){
-		System.out.println("in db"+d);
-		return 7;
-	}
+interface Doorzichtig{
+	static void kijken();
 }
 
 
